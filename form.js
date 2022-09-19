1b012922c5852f67a9dac5bf393581d3c405ea51
Ankita Chaurasia <ankitachaurasia1998@gmail.com>
2022-09-19 15:35:55 -0400

    Form Commit II

--- form.js
+++ form.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function () {
     getListAPI();
+    // callEditAPI();
+    // callDeleteAPI();
   });
 
 function getListAPI(){
@@ -23,7 +25,7 @@ function getListAPI(){
                         user += '<td>' + value.email + '</td>';
                         user += '<td>' + value.dateOfAppointment + '</td>';
 
-                        user += '<td><a href="" onclick="editAPI">Edit</a></td>';
+                        user += '<td><a href="" onclick="editAPI()">Edit</a></td>';
                         user += '<td><a href="" onclick="deleteAPI()">Delete</a></td>';
 
                         user += '</tr>';
@@ -38,7 +40,14 @@ function getListAPI(){
 }
 
 function deleteAPI(){
-    fetch('https://14dc3f6a-7cbf-47e9-9d27-81d7780f8227.mock.pstmn.io/delete', { method: 'DELETE'})
+    callDeleteAPI();
+}
+
+function callDeleteAPI(){
+    fetch('https://14dc3f6a-7cbf-47e9-9d27-81d7780f8227.mock.pstmn.io/delete', { method: 'DELETE',
+    headers: {
+        'Content-Type': 'text/plain'
+        }})
     .then(response => {
           if (response.status >= 200 && response.status < 300) {
             alert("Entry deleted successfully.");
@@ -51,3 +60,42 @@ function deleteAPI(){
             console.error('There was an error!', error);
           }); 
 }
+
+function editAPI(){
+    callEditAPI();
+}
+
+function callEditAPI(){
+    fetch('https://14dc3f6a-7cbf-47e9-9d27-81d7780f8227.mock.pstmn.io/edit', { method: 'GET'})
+    .then(async response => {
+        const row = await response.json();
+        // check for error response
+        if (!response.ok) {
+            // get error message from body or default to response statusText
+            const error = (row && row.message) || response.statusText;
+            return Promise.reject(error);
+        }  else{
+            $.each(row, function (key, value) {
+
+                $('#name').append(value.name);
+                $('#email').append(value.email);
+                $('#phNumber').append(value.phoneNumber);
+                $('#dateOfAppointment').append(value.dateOfAppointment);
+    
+              });
+        }
+    }).catch(error => {
+        this.setState({ errorMessage: error.toString() });
+        console.error('There was an error!', error);
+    });
+}
+
+function saveAPI(){
+   
+     fetch('https://cf328215-07aa-4d32-86f4-517ee2d6c520.mock.pstmn.io/saveMortgage', {
+      method: 'POST'}).then((response)=> {       
+             if (response.status >= 200 && response.status < 300) {                    
+                 console.log(response);
+                 alert("Entry saved successfully");
+            }});
+        }
